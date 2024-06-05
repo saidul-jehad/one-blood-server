4663d7ebc84301f297193dfebc0da5f879d3c408
saidul24 <jihadh32978@gmail.com>
2024-06-05 21:24:31 +0600

    added update method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require("cors")
 const jwt = require('jsonwebtoken')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -54,12 +54,103 @@ async function run() {
 
 
 
+
         // donationRequest related api
+
+
+        app.get('/all-donation-request', async (req, res) => {
+            const result = await donationRequestCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        app.get('/donation-request', async (req, res) => {
+            const query = {
+                donation_status: 'pending'
+            }
+            const result = await donationRequestCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.get('/donation-request/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                donation_status: 'pending',
+                _id: new ObjectId(id)
+            }
+            const result = await donationRequestCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.get('/donation-request-byId/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await donationRequestCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.patch('/donate/:id', async (req, res) => {
+            const id = req.params.id
+            const donorInfo = req.body
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    donation_status: "inprogress",
+                    ...donorInfo
+                },
+            };
+            const result = await donationRequestCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.patch('/update-donation-request/:id', async (req, res) => {
+            const id = req.params.id
+            const newDonorInfo = req.body
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    ...newDonorInfo
+                },
+            };
+            const result = await donationRequestCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+
+        app.patch('/done-cancel/:id', async (req, res) => {
+            const id = req.params.id
+            const status = req.query.status
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    donation_status: status
+                },
+            };
+            const result = await donationRequestCollection.updateOne(filter, updateDoc)
+            res.send(result)
+
+        })
+
+
+        app.delete('/delete-request/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+
+            const result = await donationRequestCollection.deleteOne(filter)
+            res.send(result)
+
+        })
+
+
+
         app.post('/donation-request', async (req, res) => {
             const requestInfo = req.body
             const result = await donationRequestCollection.insertOne(requestInfo)
             res.send(result)
         })
+
 
 
 
